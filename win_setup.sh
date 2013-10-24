0b3e04ec8b75c0b5d69068db7b6ec1c7b0f285a3
Nori <nori755@gmail.com>
2013-10-24 23:42:38 +0900

    Rename vimrc in windows.

--- win_setup.sh
+++ win_setup.sh
@@ -4,7 +4,7 @@ dir=~/vimfiles/bundle/vundle
 if [ ! -e $dir ]; then
     git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 fi
-cp .vimrc ~/_vimrc
-cp .gvimrc ~/_gvimrc
+cp .vimrc ~/.vimrc
+cp .gvimrc ~/.gvimrc
 cp -r template ~/vimfiles
 vim +BundleInstall +qall

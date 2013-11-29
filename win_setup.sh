0252326dede0932c9041440e2a228729295e42f7
nori <nori755@gmail.com>
2013-11-29 23:57:36 +0900

    Fix wrong path.

--- win_setup.sh
+++ win_setup.sh
@@ -2,7 +2,7 @@
 
 dir=~/vimfiles/bundle/vundle
 if [ ! -e $dir ]; then
-    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    git clone https://github.com/gmarik/vundle.git ~/vimfiles/bundle/vundle
 fi
 cp .vimrc ~/.vimrc
 cp .gvimrc ~/.gvimrc

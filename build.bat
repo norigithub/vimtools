860f171de581ace1f26d10f936ffda4074ac494f
Nori <nori755@gmail.com>
2015-07-22 22:28:44 +0900

    Change build   env to MinGW.

--- build.bat
+++ build.bat
@@ -1,13 +1,11 @@
 ::Install gVim with Self-installing executable   gvim##.exe, Before execute this bat file. 
 ::This bat will do only update binaries and runtimes vimorig.  
 ::Set up compile environment.
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvarsx86_amd64.bat" 
 
 ::Define variables.
 set PREFIX=D:\Program\Vim\vim74
-set SDK_INCLUDE_DIR=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A\Include
-set WINVER=0x0500
-set CPU=AMD64
+echo off
+set PATH=D:\program\mingw-w64\x86_64-5.1.0-posix-seh-rt_v4-rev0\mingw64\bin;%PATH%
 set MBYTE=yes
 set IME=yes
 set GIME=yes
@@ -20,19 +18,14 @@ set PYTHON3=D:\Program\Python34
 set DYNAMIC_PYTHON3=yes
 set PYTHON3_VER=34
 
-::Make Vim.
-call nmake -f Make_mvc.mak clean
-call nmake -f Make_mvc.mak
+call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=no clean
+call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=yes clean
 
-::Copy binary.
-copy /Y vim.exe %PREFIX%
-
-::Make GVim.
-set GUI=yes
-call nmake -f Make_mvc.mak clean
-call nmake -f Make_mvc.mak
+call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=no
+call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=yes
 
 ::Copy binaries.
+copy /Y vim.exe %PREFIX%
 copy /Y gvim.exe %PREFIX%
 copy /Y vimrun.exe %PREFIX%
 copy /Y xxd\xxd.exe %PREFIX%

b10d361b9918b5017a73abd7df7f933958caf3e4
Nori <nori755@gmail.com>
2017-01-03 22:47:39 +0900

    Change git path.

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 cd %USERPROFILE%
-call D:\Program\Git\bin\git.exe clone https://github.com/gmarik/Vundle.vim.git vimfiles/bundle/Vundle.vim
+git clone https://github.com/gmarik/Vundle.vim.git vimfiles/bundle/Vundle.vim
 copy vimtools\.vimrc .vimrc
 copy vimtools\.gvimrc .gvimrc
 pause

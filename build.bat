e5335434a069be146920f052297fe0076e2f9dc5
Nori <nori755@gmail.com>
2017-05-13 19:54:04 +0900

    Remove unused build script.

--- build.bat
+++ build.bat
@@ -1,34 +1,28 @@
-::Install gVim with Self-installing executable   gvim##.exe, Before execute this bat file. 
-::This bat will do only update binaries and runtimes vimorig.  
-::Set up compile environment.
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+SET Include=%Include%;C:\Program Files\Microsoft SDKs\Windows\v7.1\Include
+
 
 ::Define variables.
-set PREFIX=D:\Program\Vim\vim8
-echo off
-set PATH=D:\program\mingw-w64\x86_64-5.1.0-win32-seh-rt_v4-rev0\mingw64\bin;%PATH%
+set PREFIX=D:\Program\Vim\vim80
+set CPU=AMD64
 set MBYTE=yes
 set IME=yes
 set GIME=yes
 set ICONV=yes
 set CSCOPE=yes
 set DEBUG=no
-set PYTHON=D:\Program\Python27
-set DYNAMIC_PYTHON=yes
-set PYTHON_VER=27
-set PYTHON3=D:\Program\Python35
+set PYTHON3=C:\Users\Nori\AppData\Local\Programs\Python\Python35
 set DYNAMIC_PYTHON3=yes
 set PYTHON3_VER=35
+set DIRECTX=yes
+nmake -f Make_mvc.mak  GUI=no clean
+nmake -f Make_mvc.mak  GUI=no
 
-call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=no clean
-call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=no
-
-call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=yes clean
-call mingw32-make -f Make_ming.mak ARCH=x86-64 GUI=yes
+nmake -f Make_mvc.mak  GUI=yes clean
+nmake -f Make_mvc.mak  GUI=yes
 
 ::Copy binaries.
-copy /Y vim.exe %PREFIX%
-copy /Y gvim.exe %PREFIX%
-copy /Y vimrun.exe %PREFIX%
+copy /Y *.exe %PREFIX%
 copy /Y xxd\xxd.exe %PREFIX%
 
 ::Copy dll.

514c28643b1733d2756384bb535c5cdf9adf849c
Nori <nori755@gmail.com>
2020-02-06 22:39:23 +0900

    Add script files to install dev files which building Python needs.

---
+++ apt_python37.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#For Debian
+sudo apt install \
+                libbz2-dev \
+                libdb-dev \
+                libgdbm-dev \
+                libffi-dev \
+                uuid-dev \
+                liblzma-dev \
+                libncursesw5-dev \
+                libssl-dev \
+                libsqlite3-dev \
+                libreadline-dev \
+                zlib1g-dev
---
+++ yum_python37.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#For CentOS
+sudo yum install \
+                bzip2-devel \
+                libdb-devel \
+                gdbm-devel \
+                libffi-devel \
+                libuuid-devel \
+                xz-devel \
+                ncurses-devel \
+                openssl-devel \
+                sqlite-devel \
+                readline-devel \
+                zlib-devel

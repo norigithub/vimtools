b14df8e81978038f21bf1dfe42f7732799bd679b
nori <nori755@gmail.com>
2020-07-06 19:30:15 +0900

    Add Python38 build libs.

---
+++ apt_python38.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#For Debian 
+sudo apt install libreadline-dev \
+                    libncursesw5-dev \
+                    libssl-dev \
+                    libsqlite3-dev \
+                    libgdbm-dev \
+                    libbz2-dev \
+                    liblzma-dev \
+                    zlib1g-dev \
+                    uuid-dev \
+                    libffi-dev \
+                    libdb-dev

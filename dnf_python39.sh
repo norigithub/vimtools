1eeb2cc8d9bc096217e890302fa7b222648ceeb0
Nori <nori755@gmail.com>
2020-12-06 23:48:25 +0900

    Add Python39 build libs.

---
+++ dnf_python39.sh
@@ -0,0 +1,13 @@
+dnf install\
+            bzip2-devel\
+            expat-devel\
+            gdbm-devel\
+            ncurses-devel\
+            openssl-devel\
+            readline-devel\
+            sqlite-devel\
+            tk-devel\
+            xz-devel\
+            libuuid-devel\
+            zlib-devel\
+            libffi-devel

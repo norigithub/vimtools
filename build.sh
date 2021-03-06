2990ab373d3e643d1edfd6e432aa2a1094546efb
nori <nori755@gmail.com>
2021-03-06 22:48:30 +0900

    Update build option.

--- build.sh
+++ build.sh
@@ -2,9 +2,8 @@
 make clean
 make distclean
 ./configure --prefix=/opt/vim --enable-multibyte\
-            --enable-pythoninterp=dynamic --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-python3interp=dynamic --with-python3-config-dir=/opt/python3/lib/python3.5/config-3.5m \
-            vi_cv_path_python3=/opt/python3/bin/python3
+            --enable-pythoninterp=dynamic --with-python-config-dir=/usr/lib64/python2.7/config \
+            --enable-python3interp=dynamic --with-python3-config-dir=/opt/python3/lib/python3.8/config-3.8-x86_64-linux-gnu
+#             vi_cv_path_python3=/opt/python3/bin/python3
 make
 sudo make install
-
